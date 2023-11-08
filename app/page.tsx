0e445301b12a6ac3cbed9f934b0aa4bb335a153b
heeryoung <winterbooks@naver.com>
2023-11-08 15:25:04 +0900

    add: 메인 이미지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
+import HappyNewYearImage from '@/public/happy_new_year.png';
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <Container maxWidth="sm" fixed>
+    <Container
+      fixed
+      maxWidth="sm"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        py: 5,
+      }}
+    >
       <Typography variant="h3">초대합니다</Typography>
+      <Image
+        src="/happy_new_year.png"
+        width={400}
+        height={400}
+        quality={100}
+        alt="Happy new year"
+        style={{ borderRadius: '50%' }}
+      />
     </Container>
   );
 }
