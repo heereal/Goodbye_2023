3d38fa70f58fe70d7e7ba207e252ea3fdfb52d4e
heeryoung <winterbooks@naver.com>
2023-11-08 17:33:46 +0900

    add: desc

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,15 @@ export default function Page() {
         <Typography variant="h4" sx={{ pt: 2 }}>
           연말 파티에 초대합니다.
         </Typography>
+        <Typography variant="subtitle2" align="center">
+          2023년 연말 20대 마지막 밤을 맞이하여 귀하의 참석을 요청드립니다.
+          <br />
+          부디 자리에 참석하여 빛내 주시기를 바랍니다.
+        </Typography>
         <List disablePadding>
           <ListItemWithText label="장소" value="인계동 어딘가의 파티 룸" />
-          <ListItemWithText label="일시" value="2023년 12월 23일 토요일" />
+          <ListItemWithText label="날짜" value="2023년 12월 23일 토요일" />
+          <ListItemWithText label="시간" value="오후 8시" />
           <ListItemWithText
             label="드레스 코드"
             value="장례식 드레스 with Black"
