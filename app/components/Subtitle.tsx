904d29177be0348bba2880a9852e0bd0e9593205
heeryoung <winterbooks@naver.com>
2023-11-09 15:15:53 +0900

    fix: subtitle 띄어쓰기 추가

--- app/components/Subtitle.tsx
+++ app/components/Subtitle.tsx
@@ -5,7 +5,7 @@ export default function Subtitle() {
     <Typography variant="subtitle2" align="center">
       <span>
         2023년 연말 20대 마지막 밤을 맞이하여 귀하의 참석을 요청드립니다.
-      </span>
+      </span>{' '}
       <span>부디 자리에 참석하여 빛내 주시기를 바랍니다.</span>
     </Typography>
   );
