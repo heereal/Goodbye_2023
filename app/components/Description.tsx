96c0b92e5653f652582e8c8c0e222f494dba9ad7
heeryoung <winterbooks@naver.com>
2023-12-23 14:50:33 +0900

    fix: 모임 시간 변경

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -26,7 +26,7 @@ export default function Description() {
         }
       />
       <ListItemWithText label="날짜" value="2023년 12월 23일 토요일" />
-      <ListItemWithText label="시간" value="오후 8시" />
+      <ListItemWithText label="시간" value="오후 6시" />
       <ListItemWithText label="드레스 코드" value="특이한 옷" />
       <ListItemWithText
         label="일정"
