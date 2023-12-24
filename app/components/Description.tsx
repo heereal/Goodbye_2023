ba83554527ea0bebfd86d759ae4e1210a6a5ea01
heeryoung <winterbooks@naver.com>
2023-12-24 22:33:24 +0900

    fix: 텍스트 수정

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -2,11 +2,11 @@ import { List, Typography } from '@mui/material';
 import ListItemWithText from './ListItemWithText';
 
 const eventList = [
-  '파티룸 꾸미기',
-  '케이크 커팅식',
+  '저녁 식사',
+  '파티룸 꾸미고 사진 찍기',
   '랜덤 선물 증정식',
-  '사진 찍기',
-  '보드게임 및 방방 타기',
+  '케이크 커팅식',
+  '방방, 노래방, 보드게임',
 ];
 
 export default function Description() {
@@ -27,7 +27,7 @@ export default function Description() {
       />
       <ListItemWithText label="날짜" value="2023년 12월 23일 토요일" />
       <ListItemWithText label="시간" value="오후 6시" />
-      <ListItemWithText label="드레스 코드" value="특이한 옷" />
+      {/* <ListItemWithText label="드레스 코드" value="편안한 옷" /> */}
       <ListItemWithText
         label="일정"
         align="column"
--- app/components/Subtitle.tsx
+++ app/components/Subtitle.tsx
@@ -2,11 +2,13 @@ import { Typography } from '@mui/material';
 
 export default function Subtitle() {
   return (
-    <Typography variant="subtitle2" align="center">
-      <span>
-        2023년 연말 20대 마지막 밤을 맞이하여 귀하의 참석을 요청드립니다.
-      </span>{' '}
-      <span>부디 자리에 참석하여 빛내 주시기를 바랍니다.</span>
-    </Typography>
+    <div>
+      <Typography variant="subtitle2" align="center">
+        2023년 연말을 맞이하여 귀하의 참석을 요청드립니다.
+      </Typography>
+      <Typography variant="subtitle2" align="center">
+        부디 자리에 참석하여 빛내 주시기를 바랍니다.
+      </Typography>
+    </div>
   );
 }
